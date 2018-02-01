1e001b9c70993bb764b19170d17b3f05b62af2b8
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-01 17:25:49 -0200

    feat: Provider to App component
    created store with reducers and applied middleware to async requests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import { List } from './containers'
-
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import reducers from './redux-flow/reducers'
+import { createStore, applyMiddleware } from 'redux'
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk)
+)
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div>
         <List />
       </div>
+      </Provider>
     );
   }
 }
