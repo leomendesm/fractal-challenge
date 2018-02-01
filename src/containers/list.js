4710478567faf893aa12b6440ef245a6780e3241
Leonardo Mendes <leo.mi.me@gmail.com>
2018-02-01 17:27:54 -0200

    feat: Add fetched list to list container
    Added mapState and mapDispatch to props and connected component to redux provider
    
    [Ticket: X]

--- src/containers/list.js
+++ src/containers/list.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 import { Beer } from '../components'
 import styles from '../stylesheets/containers/list.scss'
-
+import { connect } from 'react-redux'
+import { FetchBeerList, toggleBeerDescription } from '../redux-flow/reducers/beer/action-creators'
 class List extends Component {
-  render() { 
-    return ( 
-      <div className={styles.list}>
-        <Beer name="Beer" tagline="a nice beer" />
-        <Beer name="Beer" tagline="a nice beer" />
-        <Beer name="Beer" tagline="a nice beer" />
-        <Beer name="Beer" tagline="a nice beer" />
-      </div> 
-    )
+  componentWillMount(){
+    this.props.FetchBeerList();
+  }
+  render() {
+    if(this.props.beer.isFetching === false)
+      return ( 
+        <div className={styles.list}>
+        {this.props.beer.data.map(b =>
+          <Beer name={b.name} tagline={b.tagline} /> )
+        }
+        </div>
+      )
+    else
+      return (<div>Buscando dados</div>) 
   }
 }
- 
-export default List
+
+const mapStateToProps = state => ({
+  beer: state.beer
+})
+
+const mapDispatchToProps = dispatch => ({
+  toggleBeerDescription: index => e => {
+    dispatch(toggleBeerDescription(index))
+  },
+  FetchBeerList: () => {
+    dispatch(FetchBeerList(dispatch))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(List)
